c58a2c6a3f126e8ce064fb64e8bac82c7e6e6163
R3VV1L <r3vv1l@mail.ru>
2022-09-19 19:38:07 +0300

    mbp

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,7 +2,22 @@ const Homepage = () => {
     return (
         <div>
             <h1>Homepage</h1>
-            <p className="text_homepage">Text</p>
+            <div class='grid'>
+                <div>123456</div>
+                <div>78910</div>
+                <div>123456</div>
+                <div>78910</div>
+                <div>123456</div>
+                <div>78910</div>
+                <div>123456</div>
+                <div>78910</div>
+                <div>123456</div>
+                <div>78910</div>
+                <div>123456</div>
+                <div>78910</div>
+            </div>
+            
+            {/*<p className="text_homepage">Text</p>*/}
         </div>
     )
 }
